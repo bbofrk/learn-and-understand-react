f473f17668b744475710815ca5c0470861bb5234
bbofrk <mingliwanglau@gmail.com>
2015-10-03 14:37:28 +1300

    restart

--- dropdown/src/button.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-module.exports = React.createClass({
-  render: function() {
-    return <button type="button" name="button" className={"btn " + this.props.className} onClick={this.props.whenClicked}>
-      {this.props.title}
-      <span className={this.props.subTitleClassName}>{this.props.subTitle}</span>
-    </button>
-  }
-});
